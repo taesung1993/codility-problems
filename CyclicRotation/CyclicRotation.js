a6923b010baf51d102e17d82f3fbcf48876c273a
Taesung Yun <cheonyulin@gmail.com>
2023-11-26 13:39:13 +0900

    Task Score: 100% | Correctness Score: 100%

--- CyclicRotation/CyclicRotation.js
+++ CyclicRotation/CyclicRotation.js
@@ -1,19 +1,19 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution(A, K) {
-    const rotate = (list) => {
-        const result = [];
+function rotate(A) {
+    const result = [];
 
-        for(let i=0; i<list.length; i++) {
-            const index = (i + 1) % list.length;
-            result[index] = list[i];
-        }
-
-        return result;
+    for(let i=0; i<A.length; i++) {
+        const index = (i + 1) % A.length;
+        result[index] = A[i];
     }
 
-    let answer = A;
+    return result;
+}
+
+function solution(A, K) {
+    let answer = [...A];
 
     for(let i=0; i<K; i++) {
         answer = rotate(answer);
