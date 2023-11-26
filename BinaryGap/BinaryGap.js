ef7c2003a4acd7ff3917bcaa3cc1574c7265161c
Taesung Yun <cheonyulin@gmail.com>
2023-11-26 13:34:33 +0900

    Task Score: 100% | Correctness Score: 100%

--- BinaryGap/BinaryGap.js
+++ BinaryGap/BinaryGap.js
@@ -2,20 +2,19 @@
 // console.log('this is a debug message');
 
 function solution(N) {
+    // Implement your solution here
     const binaryCodes = N.toString(2).split('');
-    const memory = [];
+    let beforeIndex = -1;
     let max = 0;
 
     for(let i=0; i<binaryCodes.length; i++) {
         const num = +binaryCodes[i];
-        
+
         if(num === 1) {
-            if(memory.length < 1) {
-                memory.push(i);
-            } else {
-                max = Math.max(max, i - memory.slice(-1)[0] - 1);
-                memory.push(i);
+            if(beforeIndex > -1) {
+                max = Math.max(max, i - beforeIndex - 1);
             }
+            beforeIndex = i;
         }
     }
 
